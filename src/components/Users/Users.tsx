fb14cbde6d3d93aebd5c703febb319565a7b159a
Sergei Lukashin <lsv1387@gmail.com>
2023-05-10 15:29:20 +0300

    fix to pagination MUI

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,11 +3,11 @@ import style from './Users.module.css'
 import Button from "../Button/Button";
 import {UsersPropsType} from "./UsersContainer";
 import {UsersPageType} from "../../redux/stateTypes";
+import Pagination from '@mui/material/Pagination';
+
 import axios from "axios";
 
-
 class Users extends React.Component<UsersPropsType, UsersPageType> {
-
 
   componentDidMount() {
     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
@@ -15,36 +15,35 @@ class Users extends React.Component<UsersPropsType, UsersPageType> {
         this.props.setUsers(res.data.items)
         this.props.setTotalUserCountAC(res.data.totalCount)
       })
-
   }
 
   onClickSetCurrentPage = (currentPage: number) => {
+    // debugger
     this.props.setCurrentPage(currentPage)
     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`)
       .then(res => this.props.setUsers(res.data.items))
   }
 
+
   render() {
     let pagesCount = Math.ceil(this.props.totalCount / this.props.pageSize)
-    let pages = []
+    let pages: number[] = []
 
     for (let i = 1; i <= pagesCount; i++) {
       pages.push(i)
     }
 
-    let curP = this.props.currentPage;
-    let curPF = ((curP - 5) < 0) ? 0 : curP - 5;
-    let curPL = curP + 5;
-    let slicedPages = pages.slice(curPF, curPL);
-
 
     return (<React.Fragment>
         <div className={style.buttons}>
-          {slicedPages.map((p, i) =>
-            <span key={i}
-                  className={this.props.currentPage === p ? style.selected : ''}
-                  onClick={() => this.onClickSetCurrentPage(p)}
-            >{p}</span>)}
+          <Pagination count={pages.length}
+                      onChange={(e, page) => {
+                        this.onClickSetCurrentPage(page)
+                      }}
+                      size="small"
+                      color="secondary"
+                      variant="outlined" shape="rounded"
+          />
         </div>
         {this.props.usersPage.usersData.map(user => {
           const onClickFollowUser = () => {
